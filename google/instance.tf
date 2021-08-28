45cef32e38af97168f4dd2d0b1c39ed94437e98e
Robert Rodriguez <robert.rodriguez.jr.70@gmail.com>
2021-08-28 12:50:18 -0700

    Update instance.tf

--- google/instance.tf
+++ google/instance.tf
@@ -1,6 +1,6 @@
 provider "google" {
   credentials = file("mygcp-creds.json")
-  project = "MyVMs"
+  project = "myvms-324222"
   region  = "us-west2"
   zone    = "us-west2-a"
 }
@@ -8,14 +8,16 @@ provider "google" {
 resource "google_compute_instance" "roberts_vm" {
   name         = "virtual-machine-from-terraform"
   machine_type = "e2-micro"
-  zone         = "us-west2-a"
-  boot_disk{
-    initialize_parms{
-      image = "debain-cloud/demain-9"
+  boot_disk {
+    initialize_params {
+      image = "centos-7-v20210817"
     }
   }
 
-network_interface{
+network_interface {
   network = "default"
   access_config{}      // enable public IP
 }
+
+}
+
